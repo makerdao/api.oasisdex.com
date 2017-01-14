92006332d31edde8e78e4c9496179a72f1e1c0e2
Mikael Brockman <mikael@brockman.se>
2017-01-14 06:16:20 +0200

    extract getAllLogs function

--- fetch.js
+++ fetch.js
@@ -2,6 +2,32 @@ var config = require("./config.js")
 var etherscan = require("./etherscan.js")
 var moment = require("moment")
 
+function getAllLogs(blockNumber, address) {
+  let allLogs = []
+  let previousBlockNumber = blockNumber
+
+  function next() {
+    return etherscan({
+      module    : "logs",
+      action    : "getLogs",
+      fromBlock : +previousBlockNumber + 1,
+      toBlock   : "latest",
+      address   : address,
+    }).then(logs => {
+      if (logs.length == 0) {
+        return allLogs
+      } else {
+        let blockNumber = +logs[logs.length - 1].blockNumber
+        previousBlockNumber = blockNumber
+        allLogs = [...allLogs, ...logs]
+        return next()
+      }
+    })
+  }
+
+  return next()
+}
+
 module.exports = txhash => {
   var currentMoment = moment(new Date)
   var legacyMarket = /^0x5a35c21f/.test(txhash)
@@ -14,37 +40,18 @@ module.exports = txhash => {
       tag     : openingBlockNumber,
       boolean : "true",
     }).then(openingBlock => {
-      let marketLogs = []
-      let previousBlockNumber = openingBlockNumber
-      return next()
-
-      function next() {
-        return etherscan({
-          module    : "logs",
-          action    : "getLogs",
-          fromBlock : +previousBlockNumber + 1,
-          toBlock   : "latest",
-          address   : marketAddress,
-        }).then(newMarketLogs => {
-          if (newMarketLogs.length == 0) {
-            return etherscan.rpc("eth_getBlockByNumber", {
-              tag     : "0x" + previousBlockNumber.toString(16),
-              boolean : true,
-            }).then(lastActivityBlock => { 
-              return {
-                marketLogs,
-                openingBlock,
-                lastActivityBlock,
-              }
-            })
-          } else {
-            let blockNumber = +newMarketLogs[newMarketLogs.length - 1].blockNumber
-            previousBlockNumber = blockNumber
-            marketLogs = [...marketLogs, ...newMarketLogs]
-            return next()
+      return getAllLogs(openingBlockNumber, marketAddress).then(marketLogs => {
+        return etherscan.rpc("eth_getBlockByNumber", {
+          tag     : marketLogs[marketLogs.length - 1].blockNumber,
+          boolean : true,
+        }).then(lastActivityBlock => { 
+          return {
+            marketLogs,
+            openingBlock,
+            lastActivityBlock,
           }
         })
-      }
+      })
     })
   })
 }
